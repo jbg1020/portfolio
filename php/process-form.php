d41a7f370a92a443811d9d9f3d679516a973e6fa
jbg1020 <jbg1020@gmail.com>
2020-01-22 13:50:54 -0800

    testing contact with remote

--- php/process-form.php
+++ php/process-form.php
@@ -6,11 +6,10 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
     $message = $_REQUEST['message'];
       
     // Set your email address where you want to receive emails. 
-    $to = 'jbg1020@gmail.com';
+    $to = 'jbg1020@hhhkjhkjjh.com';
       
     $subject = 'Contact Request From Website';
     $headers = "From: ".$name." <".$email."> \r\n";
-      
     $send_email = mail($to,$subject,$message,$headers);
     
     echo ($send_email) ? 'success' : 'error';
