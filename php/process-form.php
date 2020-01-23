a7633c1c4b8a4ab3c0ba4d6b3202ccd7882bb5d2
jbg1020 <jbg1020@gmail.com>
2020-01-23 13:17:43 -0800

    testing contact form

--- php/process-form.php
+++ php/process-form.php
@@ -1,14 +1,18 @@
 <?php
 
-if (isset($_POST['send'])) {
+if (isset($_POST['name'],$_POST['email'])) {
+      
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['comment'];
     $to = 'jbg@jbradygreco.com';
+      
     $subject = 'Contact Request From Website';
     $headers = "From: ".$name." <".$email."> \r\n";
-
-    mail($to,$subject,$message,$headers);
+    $send_email = mail($to,$subject,$message,$headers);
+    
+    echo (!$send_email) ? 'success' : 'error';
+      
 }
 
 ?>
