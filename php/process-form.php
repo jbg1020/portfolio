8b515fbcc9789a41e1ef253dbbd27d9fed27fb83
jbg1020 <jbg1020@gmail.com>
2020-01-26 14:51:55 -0800

    changed email subject

--- php/process-form.php
+++ php/process-form.php
@@ -23,7 +23,7 @@ if (array_key_exists('email', $_POST)) {
     $mail->addAddress('jbg@jbradygreco.com', 'Brady');
 
     if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-        $mail->Subject = 'Contact Request From Website';
+        $mail->Subject = 'jbradygreco.com NEW MESSAGE';
         $mail->isHTML(false);
         $mail->Body = <<<EOT
 Email: {$_POST['email']}
